bf7ae038e98281f1673e6f67b0273136023277c0
Tomasz Czerkas <tomeqc@gmail.com>
2026-01-21 19:51:06 -0500

    declaration file

--- src/components/Scene/ViewHelper.d.ts
+++ src/components/Scene/ViewHelper.d.ts
@@ -1,18 +1,16 @@
-declare module 'three/examples/jsm/helpers/ViewHelper.js' {
-  import { Camera, Object3D, WebGLRenderer, Vector3 } from 'three';
+import { Camera, Object3D, WebGLRenderer, Vector3 } from 'three';
 
-  export class ViewHelper extends Object3D {
-    isViewHelper: boolean;
-    animating: boolean;
-    center: Vector3;
+export class ViewHelper extends Object3D {
+  isViewHelper: boolean;
+  animating: boolean;
+  center: Vector3;
 
-    constructor(camera: Camera, domElement?: HTMLElement);
+  constructor(camera: Camera, domElement?: HTMLElement);
 
-    render(renderer: WebGLRenderer): void;
-    handleClick(event: PointerEvent): boolean;
-    update(delta: number): void;
-    setLabels(labelX?: string, labelY?: string, labelZ?: string): void;
-    setLabelStyle(font?: string, color?: string, radius?: number): void;
-    dispose(): void;
-  }
+  render(renderer: WebGLRenderer): void;
+  handleClick(event: PointerEvent): boolean;
+  update(delta: number): void;
+  setLabels(labelX?: string, labelY?: string, labelZ?: string): void;
+  setLabelStyle(font?: string, color?: string, radius?: number): void;
+  dispose(): void;
 }
